72bcfca581bc4da8684a5e44e5f08867fe08976f
apple <apple@appledeMacBook-Pro.local>
2018-12-14 17:53:25 +0800

    管理系统
    未来酒店管理系统

---
+++ Future/urls.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import os
+from handlers import BaseHandler
+
+from handlers import MyOrders
+from tornado.web import StaticFileHandler
+urls = [
+    (r"/api/orders",MyOrders.OrdersHandlers),
+    (r"/(.*)", StaticFileHandler,
+        dict(path=os.path.join(os.path.dirname(__file__), "html"), default_filename="index.html"))
+
+
+
+
+
+
+
+
+
+
+
+
+]
